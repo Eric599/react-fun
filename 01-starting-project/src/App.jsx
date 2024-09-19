062cb47c679b15763166a7cb4b85a790ddb9baec
Eric599 <ericcuenat@erics-mbp.home>
2024-09-18 22:06:25 -0300

    optimizing solution, learn useMemo

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,6 +1,22 @@
 import headerImage from './assets/react-core-concepts.png'
-import componentsImage from './assets/components.png'
+import {CORE_CONCEPTS} from "./data";
+import {useMemo} from "react";
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+
+// we use the useMemo here so .map is not called every time we render
+// instead of putting the mapping code in the component we put it here and simply call this function to display our list of components
+const conceptMapping = useMemo(() => {
+  return CORE_CONCEPTS.map(concept => {
+    return (
+        <CoreConcept
+            title={concept.title}
+            description={concept.description}
+            image={concept.image}
+        />
+    )
+  })
+}, [])
 
 const getRandomInt = max => {
   return Math.floor(Math.random() * (max +1))
@@ -38,16 +54,7 @@ const App = () => {
           <section id="core-concepts">
             <h2>Core concepts</h2>
             <ul>
-              <CoreConcept
-                title="Components"
-                description="The core UI builidng block"
-                image={componentsImage}
-              />
-              <CoreConcept
-                  title="Props"
-                  description="The core UI builidng block"
-                  image={componentsImage}
-              />
+              {conceptMapping}
             </ul>
           </section>
           <h2>Time to get started!</h2>
